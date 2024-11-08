72906b94b07ab1ac3d36e2bc7d4b6afa039bb1e6
Liande Guo <104030726+liandeguo@users.noreply.github.com>
2024-11-08 22:26:31 +0100

    basic logic added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,21 @@
+<script>
+	function onKeyDown(e) {
+		switch (e.keyCode) {
+			case 13:
+				alert(document.querySelector('#url').value);
+				break;
+		}
+	}
+</script>
+
 <main
 	class="bg flex h-screen w-screen flex-col items-center justify-center bg-[url('background.webp')] bg-cover bg-center"
 >
 	<input
 		type="text"
-		class="w-[40vw] rounded-full bg-gray-400 bg-opacity-40 bg-clip-padding px-4 py-4 text-lg text-white backdrop-blur-sm max-sm:w-[80vw]"
+		id="url"
+		on:keydown={onKeyDown}
+		class="w-[40vw] rounded-full bg-gray-400 bg-opacity-40 bg-[url('return.svg')] bg-scroll bg-clip-padding bg-no-repeat px-6 py-4 text-lg text-white backdrop-blur-sm max-sm:w-[80vw]"
 	/>
 </main>
 
@@ -11,5 +23,9 @@
 	textarea:focus,
 	input:focus {
 		outline: none;
+	}
+
+	input {
+		background-position: right 20px center;
 	}
 </style>
